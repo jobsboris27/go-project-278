792f6730034c0a78a16ae8fe2f698e5ec2834701
Boris <jobsboris27@yandex.ru>
2026-02-20 09:39:40 +0300

    step 3

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 [![Actions Status](https://github.com/jobsboris27/go-project-278/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/jobsboris27/go-project-278/actions)
 
 ![CI](https://github.com/jobsboris27/go-project-278/workflows/CI/badge.svg)
+
+[![Deploy to Render](https://render.com/images/deploy-to-render-button.svg)](https://render.com/deploy)
+Демо: https://go-project-278-881v.onrender.com
--- main.go
+++ main.go
@@ -1,16 +1,36 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"os"
+	"time"
+
+	"github.com/getsentry/sentry-go"
+	sentrygin "github.com/getsentry/sentry-go/gin"
+	"github.com/gin-gonic/gin"
+)
 
 func router() *gin.Engine {
 	r := gin.New()
+
+	if dsn := os.Getenv("SENTRY_DSN"); dsn != "" {
+		if err := sentry.Init(sentry.ClientOptions{Dsn: dsn}); err != nil {
+			panic(err)
+		}
+		r.Use(sentrygin.New(sentrygin.Options{}))
+		defer sentry.Flush(2 * time.Second)
+	}
+
 	r.Use(gin.Logger(), gin.Recovery())
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(200, "pong")
+	})
+
+	r.GET("/panic", func(c *gin.Context) {
+		panic("test error for Sentry")
 	})
 	return r
 }
 
 func main() {
-	router().Run(":8080")
+	router().Run(":" + os.Getenv("PORT"))
 }
